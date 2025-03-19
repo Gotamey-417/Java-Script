e0fa8cfc7cd1d84421d95e53587f6c2b3f23e093
Sandip <sandipgautam263@gmail.com>
2025-03-19 08:07:32 +0545

    conversion

--- type conversion.js
+++ type conversion.js
@@ -1,3 +1,25 @@
-let s = "123";
-let n = Number(s);  // Converts string to number
-console.log(n);  
+// // // Converts string to number
+// // let s = "123";
+// // let n = Number(s);
+// // console.log(n);
+
+
+
+// //Converts number to string
+// let num = 123;
+// let str = String(num);
+// console.log(str);
+
+
+// //Converts boolean to number
+// let bool = true;
+// let n = Number(bool);
+// console.log(n);
+
+
+
+// //Converts boolean to string
+// let bool = true;
+// let s = String(bool);
+// console.log(s);
+
