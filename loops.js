444a12198737c2e4c8307a7b0155d0a716a2fde3
Sandip <sandipgautam263@gmail.com>
2025-03-20 06:16:49 +0545

    Loop

---
+++ loops.js
@@ -0,0 +1,23 @@
+// //for loop
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+
+// // while loop
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+//Do while loop
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
