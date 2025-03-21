885233e36bbf4acfbf942c1afd1cde327655525e
Sandip <sandipgautam263@gmail.com>
2025-03-21 22:30:52 +0545

    Itterations

--- loops.js
+++ loops.js
@@ -8,6 +8,48 @@ This loop runs a set number of times.*/
 // }
 
 
-for (let i = 1; i <= 5; i++) {
-  console.log(i);
-}
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+
+
+/*2. While Loop
+This loop continues to execute as long as the condition remains true.
+
+while (condition) {
+    // Code to execute
+}*/
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+
+/*3. Do...While Loop
+This loop will execute the code at least once, even if the condition is false.
+
+Syntax:
+
+do {
+    // Code to execute
+} while (condition);
+*/
+
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+
+
+/*Key Differences Between Loops
+Loop Type	When to Use
+for loop	When you know how many times you need to run the loop.
+while loop	When you don’t know how many times the loop should run, but it depends on a condition.
+do...while loop	When you want to run the code at least once before checking the condition.*/
