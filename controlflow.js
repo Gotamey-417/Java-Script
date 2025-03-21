a5cff0f89b5c18f2852ecb7d1580db619500a0b5
Sandip <sandipgautam263@gmail.com>
2025-03-21 22:24:05 +0545

    flow control

--- controlflow.js
+++ controlflow.js
@@ -1,29 +1,29 @@
 
-// // if statement
-// const age = 18;
-// if (age >= 18) {
-//   console.log("You are an adult.");
-// }
+// if statement
+const age = 18;
+if (age >= 18) {
+  console.log("You are an adult.");
+}
 
 
-// //if else statement
-// const score = 40;
-// if (score >= 50) {
-//   console.log("You passed.");
-// } else {
-//   console.log("You failed.");
-// }
+//if else statement
+const score = 40;
+if (score >= 50) {
+  console.log("You passed.");
+} else {
+  console.log("You failed.");
+}
 
 
 
 
-// //if...else if...else Statement
+//if...else if...else Statement
 
-// const temp = 25;
-// if (temp > 30) {
-//     console.log("It's hot.");
-// } else if (temp >= 20) {
-//     console.log("It's warm.");
-// } else {
-//     console.log("It's cold.");
-// }
+const temp = 25;
+if (temp > 30) {
+  console.log("It's hot.");
+} else if (temp >= 20) {
+  console.log("It's warm.");
+} else {
+  console.log("It's cold.");
+}
