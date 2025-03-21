c52015f5bd1df56d05fc925682f1b7a4a93a92c2
Sandip <sandipgautam263@gmail.com>
2025-03-21 22:22:32 +0545

    Itteration

--- loops.js
+++ loops.js
@@ -1,23 +1,13 @@
-// //for loop
+/*1. For Loop
+This loop runs a set number of times.*/
 
-// for (let i = 1; i <= 5; i++) {
-//   console.log(i);
+// Syntax:
+
+// for (initialization; condition; increment/decrement) {
+//     // Code to be executed
 // }
 
 
-// // while loop
-
-// let i = 1;
-// while (i <= 5) {
-//   console.log(i);
-//   i++;
-// }
-
-
-//Do while loop
-
-// let i = 1;
-// do {
-//   console.log(i);
-//   i++;
-// } while (i <= 5);
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
